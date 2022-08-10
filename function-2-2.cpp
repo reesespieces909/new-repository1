acb034ca7771e81a56e71586d0b91353b6796493
reesespieces909 <104746465+reesespieces909@users.noreply.github.com>
2022-08-10 13:54:13 +0930

    Add files via upload

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -2,12 +2,13 @@
 
 using namespace std;
 
-int binary_to_int(int binary_digits[], int number_of_digits)
-{
-int number = 0;
-for(int i=0; i<number_of_digits; i++)
-{
-number = number | (binary_digits[i] << i);
-}
-return number;
+int bin_to_int(int binary_digits[],int number_of_digits){
+
+
+    int result = 0;
+    for (int i = 0; i < number_of_digits; ++i) {
+        result *= 2;
+        result += binary_digits[i];
+    }
+    return result;
 }
--- main-2-2.cpp
+++ main-2-2.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 
 int main(){
-int array[30] = {0,0,0,0,0,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,0,1,0,1,0,0,1,0,1,1}; 
-extern int binary_to_int(int binary_digits[], int number_of_digits);
-binary_to_int(array,30);
-cout << "" << endl;
+extern int bin_to_int(int binary_digits[],int number_of_digits);
 
+
+    int array[30] = {1, 0, 1, 0, 0};
+    cout << bin_to_int(array, 5) << endl;
+    return 0;
 }
