64e071457ab56d36c4691a05c6d1de5d4e89a3a1
reesespieces909 <104746465+reesespieces909@users.noreply.github.com>
2022-10-26 14:00:16 +1030

    Add files via upload

---
+++ Employee.cpp
@@ -0,0 +1,18 @@
+#include "employee.h"
+int Employee::get_payRate()
+{
+    return this->PayRate;
+}
+void Employee::set_payRate(int payRate)
+{
+    this->payRate = payRate;
+}
+
+float Employee::get_energyLevel()
+{
+    return this->energyLevel;
+}
+void Employee::set_energyLevel(float energyLevel)
+{
+    this->energyLevel = energyLevel;
+}
